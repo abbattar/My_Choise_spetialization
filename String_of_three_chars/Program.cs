e8f660549e70539b7b3bb5fd713d97394bed7c99
abbattar <abbattar@mail.ru>
2022-09-29 17:53:40 +0300

    All ready

---
+++ String_of_three_chars/Program.cs
@@ -0,0 +1,24 @@
+﻿# nullable disable
+string[] RecordString()
+{
+    string[] someString = new string[5];
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Введите строку:");
+        someString[i] = Console.ReadLine();
+    }
+    return someString;
+}
+
+string[] recordString = RecordString();
+Console.Write("[");
+int i = 0;
+while(i < recordString.Length)
+{
+    if (recordString[i].Length <= 3)
+    {
+        Console.Write($" {recordString[i]}");
+    }
+    i++;
+}
+Console.WriteLine("]");
